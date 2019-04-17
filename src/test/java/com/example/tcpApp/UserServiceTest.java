82b8eb21252ad05fa1f2503802bf3938f8debe3d
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-17 16:49:20 -0400

    added userService tests

--- src/test/java/com/example/tcpApp/UserServiceTest.java
+++ src/test/java/com/example/tcpApp/UserServiceTest.java
@@ -5,17 +5,23 @@ import com.example.tcpApp.repositories.ChannelRepository;
 import com.example.tcpApp.repositories.UserRepository;
 import com.example.tcpApp.services.ChannelService;
 import com.example.tcpApp.services.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -33,24 +39,82 @@ public class UserServiceTest {
     @MockBean
     private ChannelRepository channelRepositoryMock;
 
-//    @Test
-//    public void testFindAll() {
-//        List<User> users = new ArrayList<>();
-//        User user1 = new User();
-//        user1.setFirstName("Ash");
-//        user1.setLastName("Bloxom");
-//        user1.setUsername("ashblox");
-//        User user2 = new User();
-//        user2.setFirstName("Cara");
-//        user2.setLastName("Eppes");
-//        user2.setUsername("ceppes");
-//        users.add(user1);
-//        users.add(user2);
-//
-//        when(userRepositoryMock.findAll()).thenReturn(users);
-//
-//    }
+    private User user1;
+    private User user2;
+    private List<User> users;
 
-//    adding comment to test commit
+    @Before
+    public void setUp() {users = new ArrayList<>();
+        user1 = new User();
+        user1.setId(1l);
+        user1.setFirstName("Ash");
+        user1.setLastName("Bloxom");
+        user1.setUsername("ashblox");
+        user2 = new User();
+        user2.setId(2l);
+        user2.setFirstName("Cara");
+        user2.setLastName("Eppes");
+        user2.setUsername("ceppes");
+        users.add(user1);
+        users.add(user2);
+    }
+
+    @Test
+    public void testFindAll() {
+        when(userRepositoryMock.findAll()).thenReturn(users);
+
+        assertThat(userService.findAll(), is(users));
+
+        verify(userRepositoryMock, times(1)).findAll();
+        verifyNoMoreInteractions(userRepositoryMock);
+    }
+
+    @Test
+    public void testFindById() {
+        when(userRepositoryMock.getOne(1l)).thenReturn(user1);
+
+        assertThat(userService.findById(1l), is(user1));
+
+        verify(userRepositoryMock, times(1)).getOne(1l);
+        verifyNoMoreInteractions(userRepositoryMock);
+    }
+
+    @Test
+    public void testFindByUsername() {
+        when(userRepositoryMock.findByUsername("ceppes")).thenReturn(user2);
+
+        assertThat(userService.findByUsername("ceppes"), is(user2));
+
+        verify(userRepositoryMock, times(1)).findByUsername("ceppes");
+        verifyNoMoreInteractions(userRepositoryMock);
+    }
+
+    @Test
+    public void testCreateUserSuccessful() throws Exception {
+        User user3 = new User();
+        user3.setFirstName("Kate");
+        user3.setLastName("Bayard");
+        user3.setUsername("kbayard");
+
+        when(userRepositoryMock.findByUsername(user3.getUsername())).thenReturn(null);
+        when(userRepositoryMock.save(user3)).thenReturn(user3);
+
+        assertThat(userService.create(user3), is(user3));
+
+        verify(userRepositoryMock, times(1)).findByUsername(user3.getUsername());
+        verify(userRepositoryMock, times(1)).save(user3);
+        verifyNoMoreInteractions(userRepositoryMock);
+    }
+
+    @Test
+    public void testCreateUserUnsuccessful() throws Exception {
+        when(userRepositoryMock.findByUsername(user1.getUsername())).thenReturn(user1);
+
+        assertThat(userService.create(user1), is(user1));
+
+        verify(userRepositoryMock, times(1)).findByUsername(user1.getUsername());
+        verifyNoMoreInteractions(userRepositoryMock);
+    }
+
 
 }
